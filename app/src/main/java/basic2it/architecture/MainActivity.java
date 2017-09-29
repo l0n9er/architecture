e291065ae20ce1f854eeb5d493b44646700f033d
racofix <zhoujinlongdev@163.com>
2017-09-30 00:46:02 +0800

    init repository

---
+++ app/src/main/java/basic2it/architecture/MainActivity.java
@@ -0,0 +1,13 @@
+package basic2it.architecture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
